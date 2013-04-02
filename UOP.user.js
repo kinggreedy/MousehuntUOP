3b94f43a9f7ce04f971aab4dc2f2eed6e22324a0
Chau Nguyen <dangchau1991@yahoo.com>
2013-04-02 17:35:25 +0300

    finished changes

--- UOP.user.js
+++ UOP.user.js
@@ -17,10 +17,13 @@
 // ==/UserScript==
 
 // The public prefix for this script is UOP_ . All of the outside variable and function will have this prefix.
-// init the script
+/**************INIT THE SCRIPT****************
+ * Attach the init function to DOMContentLoaded
+ * And check if the page fail to load in 15 sec.
+ */
 var documentLoadCounter = 0;
 checkDocumentState();
-window.addEventListener('DOMContentLoaded',main,false);
+window.addEventListener('DOMContentLoaded',initialization,false);
 function checkDocumentState()
 {
 	if (document.readyState == "loading")
@@ -31,15 +34,19 @@ function checkDocumentState()
 	}
 }
 
-// Global Setting Variable
-var version = "2.1";
-var S_ForceHTTPS = 1;
-var S_SecondInterval = 1;
-var S_MinuteInterval = 60;
-var S_ShortInterval = 5;
-var S_ModerateInterval= 30;
-var S_autoInterval = 1;
-// Global Constant Variable
+/**************VARIABLES*****************/
+//==========Constants==========
+//Setting Constants
+var C_version = "2.1";
+var C_versionCompatibleCode = "1";
+var C_ForceNonHTTPS = 1;
+var C_SecondInterval = 1;
+var C_MinuteInterval = 60;
+var C_ShortInterval = 5;
+var C_ModerateInterval= 30;
+var C_autoInterval = 1;
+
+//Constants
 var C_LOCATION_TIMES = [
 	{
 		name: 'Seasonal Garden',
@@ -86,24 +93,161 @@ var C_LOCATION_TIMES = [
 		color: ['#CD853F', '#8B4513', 'Skyblue', 'Blue', 'Green', 'Silver', '#B22222']
 	}
 ];
-// Global Control Variable
+//CallbackFunctions
+
+//==========Variables==========
+//Setting Variables
+var S_skin,S_auto,S_schedule,S_solve,S_server;
+var S_ads,S_pause,S_aggressive,S_delaymin,S_delaymax,S_alarm,S_alarmSrc,S_alarmStop,S_alarmStopTime,S_trapCheck,S_trapCheckTime,S_numScript;
+var S_cacheKRstr,S_serverUrl;
+
+//Object Variables
 var O_titleBar,O_titlePercentage;
 var O_huntTimer;
 var O_locationTimerParent;
 var O_mode;
-// Global Variable
+//Variables
+var user;
 
+/*******************INITIALIZATION********************/
+function initialization()
+{
+	//==========CHECK THE BROWSER LOCATIONS. Ex: facebook, login, turn, https, mobile, loaded with error,...==========
+	if (window.location.hostname.indexOf('facebook') != -1) {
+		return;
+	}
+	else if (window.location.pathname.indexOf('/login.php') != -1) //at login.php
+	{
+		if (document.getElementsByClassName('login').length == 0) // && logged in
+			location.href = "/";
+		return;
+	}
+	else if (window.location.pathname.indexOf('/turn.php') != -1) //at turn.php
+	{
+		location.href = "/";
+		return;
+	}
+	else if (getCookie("switch_to") == "mobile") //mobile mode
+	{
+		return;
+	}
+	else if ((location.pathname == "/index.php")|| (location.pathname == "/")) //at camp
+	{
+		if ((location.protocol == "https:") && (S_ForceNonHTTPS == 1)) //HTTPS
+		{
+			location.replace("http"+location.href.substr(5)); //force NON-HTTPS
+			return;
+		}
+		if (document.getElementById('campButton') == null) // no camp button aka error
+		{
+			setTimeout(function () {location.reload();},10000);
+			return;
+		}
+	}
+	
+	//==========LOAD SETTINGS==========
+	if ((!window.localStorage.UOP_versionCompatibleCode) || (window.localStorage.UOP_versionCompatibleCode != C_versionCompatibleCode))
+	{
+		window.localStorage.UOP_versionCompatibleCode = C_versionCompatibleCode;
+		window.localStorage.UOP_skin = 1;
+		window.localStorage.UOP_auto = 0;
+		window.localStorage.UOP_schedule = 1;
+		window.localStorage.UOP_solve = 0;
+		window.localStorage.UOP_server = 1;
+		
+		window.localStorage.UOP_pause = 0;
+		window.localStorage.UOP_ads = 1;
+		window.localStorage.UOP_aggressive = 2;
+		window.localStorage.UOP_delaymin = 5;
+		window.localStorage.UOP_delaymax = 60;
+		window.localStorage.UOP_alarm = 0;
+		window.localStorage.UOP_alarmSrc = "";
+		window.localStorage.UOP_alarmStop = 1;
+		window.localStorage.UOP_alarmStoptime = 600;
+		window.localStorage.UOP_trapCheck = 1;
+		window.localStorage.UOP_trapCheckTime = 3600;
+		window.localStorage.UOP_script = "";
+		
+		window.localStorage.UOP_cacheKRstr = "";
+		window.localStorage.UOP_serverUrl = "";
+	}
+	else if (window.localStorage.UOP_cacheKRstr == undefined) //better safe than sorry
+	{
+		window.localStorage.UOP_cacheKRstr = "";
+	}
 
-// Global MAIN
+	S_skin = Number(window.localStorage.UOP_skin);
+	S_auto = Number(window.localStorage.UOP_auto);
+	S_schedule = Number(window.localStorage.UOP_schedule);
+	S_solve = Number(window.localStorage.UOP_solve);
+	S_server = Number(window.localStorage.UOP_server);
+	
+	S_pause = Number(window.localStorage.UOP_pause);
+	S_ads = Number(window.localStorage.UOP_ads);
+	S_aggressive = Number(window.localStorage.UOP_aggressive);
+	S_delaymin = Number(window.localStorage.UOP_delaymin);
+	S_delaymax = Number(window.localStorage.UOP_delaymax);
+	S_alarm = Number(window.localStorage.UOP_alarm);
+	S_alarmSrc = window.localStorage.UOP_alarmSrc;
+	S_alarmStop = Number(window.localStorage.UOP_alarmStop);
+	S_alarmStopTime = Number(window.localStorage.UOP_alarmStoptime);
+	S_trapCheck = Number(window.localStorage.UOP_trapCheck);
+	S_trapCheckTime = Number(window.localStorage.UOP_trapCheckTime);
+	S_script = window.localStorage.UOP_script;
+	
+	S_cacheKRstr = window.localStorage.UOP_cacheKRstr;
+	S_serverUrl = window.localStorage.UOP_serverUrl;
+	
+	//==========INIT VARIABLES==========
+	
+	//==========CALL THE MAIN FUNCTION==========
+	main();
+}
+/*******************MAIN********************/
 function main()
 {
-	priorityFunctions();
-}
-function priorityFunctions()
-{
-	// if loaded but error: reload
-	// if not login: terminate script
-	// load the saved settings
-	// init the things
+	
 }
 
+/*******************TOOLS********************/
+function getCookie(c_name)
+{
+	if (document.cookie.length > 0)
+	{
+		var c_start = document.cookie.indexOf(c_name + "=");
+		if (c_start != -1)
+		{
+			c_start = c_start + c_name.length + 1;
+			var c_end = document.cookie.indexOf(";", c_start);
+			if (c_end == -1) c_end = document.cookie.length;
+			
+			return unescape(document.cookie.substring(c_start, c_end));
+		}
+	}
+	return null;
+}
+
+function syncUser(callbackFunction)
+{
+	var request = new XMLHttpRequest();
+	request.open("GET", "managers/ajax/abtest.php", true);
+	request.setRequestHeader("Cache-Control","no-cache, must-revalidate");
+	request.setRequestHeader("Pragma","no-cache");
+	request.setRequestHeader("If-Modified-Since","Sat, 1 Jan 2000 00:00:00 GMT");
+	request.onreadystatechange = function()
+	{
+		if (request.readyState === 4)
+		{
+			if (request.status == 200)
+			{
+				user = JSON.parse(request.responseText);
+				if (callbackFunction != null) callbackFunction();
+			}
+			else
+			{
+				location.reload();
+			}
+		}
+	};
+	request.send(null);
+}
